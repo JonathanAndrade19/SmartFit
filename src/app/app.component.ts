3e9e500e130bc535317923a3fe5be9d3381f7cc1
jonathanAndrade19 <jonathanjampamb@gmail.com>
2024-07-16 17:23:23 -0300

    fix: modification to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,13 @@ import { CardsListComponent } from "./components/cards-list/cards-list.component
 import { CommonModule } from '@angular/common';
 import { Location } from './types/location';
 import { GetUnitsService } from './services/get-units.service';
+import { LegendComponent } from './components/legend/legend.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FormsComponent, CardsListComponent, CommonModule],
+  imports: [RouterOutlet, HeaderComponent, FormsComponent, CardsListComponent, CommonModule, LegendComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
