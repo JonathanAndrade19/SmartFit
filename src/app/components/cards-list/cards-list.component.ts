a965987bab159996c3fe5634cc216005a4a500a4
jonathanAndrade19 <jonathanjampamb@gmail.com>
2024-07-16 16:31:35 -0300

    feat: create unit cards list

---
+++ src/app/components/cards-list/cards-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { GetUnitsService } from '../../services/get-units.service';
+import { Location } from '../../types/location';
+import { CardComponent } from '../card/card.component';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-cards-list',
+  standalone: true,
+  imports: [CardComponent, NgFor],
+  templateUrl: './cards-list.component.html',
+  styleUrl: './cards-list.component.scss'
+})
+export class CardsListComponent implements OnInit {
+
+  @Input() unitsList: Location[] = [];
+
+  constructor(){}
+
+  ngOnInit(): void {}
+}
--- src/app/types/location.ts
+++ src/app/types/location.ts
@@ -1,13 +1,13 @@
 export interface Location {
     id: number,
-      title: string,
-      content: string,
-      opened: boolean,
-      mask: string,
-      towel: string,
-      fountain: string,
-      locker_room: string,
-      schedules: Schedules[]
+    title: string,
+    content: string,
+    opened: boolean,
+    mask: string,
+    towel: string,
+    fountain: string,
+    locker_room: string,
+    schedules: Schedules[]
 }
 
 interface Schedules {
